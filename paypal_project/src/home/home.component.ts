e36759946056b354374ca6cdef8be917ed66b8e8
Ndithomas <ndithomas30@gmail.com>
2024-08-26 23:22:24 +0200

    social get started

--- paypal_project/src/home/home.component.ts
+++ paypal_project/src/home/home.component.ts
@@ -4,11 +4,20 @@ import { BackgroundImageComponent } from '../background-image/background-image.c
 import { PaypalIconsComponent } from '../paypal-icons/paypal-icons.component';
 import { WithdrawalComponent } from '../withdrawal/withdrawal.component';
 import { BuyersSellersComponent } from '../buyers-sellers/buyers-sellers.component';
+import { SocialComponent } from '../social/social.component';
+import { GetStartedComponent } from '../get-started/get-started.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,BackgroundImageComponent,PaypalIconsComponent,WithdrawalComponent,BuyersSellersComponent],
+  imports: [NavbarComponent,
+    BackgroundImageComponent,
+    PaypalIconsComponent,
+    WithdrawalComponent,
+    BuyersSellersComponent,
+  SocialComponent,
+  GetStartedComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
