ceeeac65b63b5a741920eff4f9a9936883c3640e
Olanrewaju Ojo <olanrewajuj@gmail.com>
2024-09-09 15:27:56 -0400

    login UI

--- paypal_project/src/login/login.component.ts
+++ paypal_project/src/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup
+
+  constructor(private fb: FormBuilder){
+    this.loginForm =this.fb.group({
+      username:[', Validators.required']
+    })
+  }
+
+  onLogin(): void {
+    if (this .loginForm.valid){
+      const {username} = this.loginForm.value;
+      
+    }
+  }
 
 }
