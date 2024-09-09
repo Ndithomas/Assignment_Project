e8603f898278c3cac9c474e27c81cbd465a77f68
Ndi Thomas <ndithomas30@gmail.com>
2024-09-10 01:51:13 +0200

    Update login.component.ts

--- paypal_project/src/login/login.component.ts
+++ paypal_project/src/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 
 @Component({
@@ -10,19 +10,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  loginForm: FormGroup
-
-  constructor(private fb: FormBuilder){
-    this.loginForm =this.fb.group({
-      username:[', Validators.required']
-    })
-  }
-
-  onLogin(): void {
-    if (this .loginForm.valid){
-      const {username} = this.loginForm.value;
-      
-    }
-  }
+  
 
 }
